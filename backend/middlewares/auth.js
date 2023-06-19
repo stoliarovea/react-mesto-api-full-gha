d18fcda68e1b1667cb8df62f8cb7af5c989617cb
stoliarovea <stoliarovea@gmail.com>
2023-06-19 21:13:25 +0300

    19.06.2023

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/unauthorized');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new Unauthorized('Unauthorized');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    throw new Unauthorized('Unauthorized');
+  }
+
+  req.user = payload;
+
+  next();
+};
