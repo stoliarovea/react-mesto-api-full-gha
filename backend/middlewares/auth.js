e190f19ceddc06ea8e91431e17ea9158436208b5
stoliarovea <stoliarovea@gmail.com>
2023-06-20 20:45:16 +0300

    20.06.2023-24

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const Unauthorized = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
@@ -12,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new Unauthorized('Unauthorized');
   }
