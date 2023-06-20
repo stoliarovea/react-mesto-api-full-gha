938298634fa8a64fbb8cf76f0103722ade8019bf
stoliarovea <stoliarovea@gmail.com>
2023-06-20 15:27:14 +0300

    20.06.2023-2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -73,7 +73,7 @@ class Api {
         method: 'DELETE',
         headers: this._headers.headers,
       }).then(this._checkResponse);
-  
+
     }
   }
 
@@ -93,7 +93,8 @@ const api = new Api({
   headers: {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      //      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDkxOTBjMTY1ZTZiMjczMDQ4MGU4ZjIiLCJpYXQiOjE2ODcyNjM3NjMsImV4cCI6MTY4Nzg2ODU2M30.oQ-iyGdB4AZ2cUGrO-o757TNtYcwu3IVPSsVoIyGOaM',
     }
   }
 })
